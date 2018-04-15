44bdccdf2fbb9a8206cc473e2fedb1f123d17671
Yuan Li <yuanli1213@Yuans-MacBook-Pro.local>
2018-04-15 13:38:00 -0700

    avoid collision by slowing down when too close to the front car

--- src/main.cpp
+++ src/main.cpp
@@ -246,6 +246,35 @@ int main() {
 
             int prev_size = previous_path_x.size();
 
+            //avoid collision
+            if (prev_size > 0) {
+              car_s = end_path_s;
+            }
+
+            bool too_close = false;
+
+            //find ref_vel to use
+            for (int i = 0; i < sensor_fusion.size(); i++) {
+              //check if car is in my lane
+              float d = sensor_fusion[i][6];
+              if (d < 2 + 4*lane && d > 2 + 4*lane - 2) {
+                double vx = sensor_fusion[i][3];
+                double vy = sensor_fusion[i][4];
+                double check_speed = sqrt(vx*vx + vy*vy);
+                double check_car_s = sensor_fusion[i][5];
+
+                //project s value outwards in time, looking at where other cars in the future
+                check_car_s += (double)prev_size * .02 * check_speed; 
+
+                //check s values greater than mine and s gap
+                if (check_car_s > car_s && check_car_s - car_s < 30) {
+                  //do some logic here, lower reference velocity so we don't crash into the car in front of us, could also flag to try changing lanes
+                  ref_vel = 29.5; //mph
+                  //too_close = true;
+                }
+              }
+            }
+
             //create a list of widely spaced (x,y) waypoints, evenly spaced at 30m
             //later we will incorporate these waypoints with a spline and fill it in with more points that control speed
             vector<double> ptsx;
