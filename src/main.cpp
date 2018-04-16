694bc43c650b4dbd6cc295e01c69bed23ccc05c8
Yuan Li <yuanli1213@Yuans-MacBook-Pro.local>
2018-04-15 17:21:07 -0700

    update comments

--- src/main.cpp
+++ src/main.cpp
@@ -204,7 +204,7 @@ int main() {
   //start in lane 1
   int lane = 1;
 
-  //have a reference velocity to target
+  //have a reference velocity to target in mph
   double ref_vel = 0.0;
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane,&ref_vel](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
